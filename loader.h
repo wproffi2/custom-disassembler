5d3e076829807645a56e311a11395b94f25e8975
warrie <wnprfftt@gmail.com>
2020-03-11 13:51:45 -0400

    init loader.h

---
+++ loader.h
@@ -0,0 +1,22 @@
+#include LOADER_H
+#define LOADER_H
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+//define classes
+class Symbol;
+
+class Symbol {
+    public:
+        enum SymbolType {
+            SYM_TYPE_UKN = 0,
+            SYM_TYPE_FUNC = 1
+        };
+
+        Symbol() : type(SYM_TYPE_UKN), name(), addr(0) {}
+        SymbolType type;
+        std::string name;
+        uint64_t addr;
+};
