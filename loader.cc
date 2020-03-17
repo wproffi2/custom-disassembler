437a97f22446edbfa62032dfd7525dd3c05fe2f2
warrie <wnprfftt@gmail.com>
2020-03-17 17:11:36 -0400

    added open_bfd function

--- loader.cc
+++ loader.cc
@@ -19,4 +19,40 @@ void unload_binary(Binary *bin) {
     }
 }
 
-//section 4.3.1 and listing 4-4
+static bfd*
+open_bfd(std::string &fname) {
+    static int bfd_inited = 0;
+    bfd *bfd_h;
+
+    if(!bfd_inited) {
+        bfd_init();
+        bfd_inited = 1;
+    }
+
+    bfd_h = bfd_openr(fname.c_str(), NULL);
+
+    if(!bfd_h) {
+        fprintf(stderr, "failed to open binary '%s' (%s)\n",
+            fname.c_str(), bfd_errmsg(bfd_get_error()));
+        
+        return NULL;
+    }
+
+    if(!bfd_check_format(bfd_h, bfd_object)) {
+        fprintf(stderr, "file '%s' does not look like an executable (%s)\n",
+            fname.c_str(), bfd_errmsg(bfd_get_error()));
+        
+        return NULL;
+    }
+
+    bfd_set_error(bfd_error_no_error);
+
+    if(bfd_get_flavour(bfd_h) == bfd_target_unknown_flavour) {
+        fprintf(stderr, "unrecognized format for binary '%s' (%s)\n",
+            fname.c_str(), bfd_errmsg(bfd_get_error()));
+
+        return NULL;
+    }
+
+    return bfd_h;
+}
